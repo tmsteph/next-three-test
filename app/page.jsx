2b8438302f0f63c7ec8381199149e75213bd3352
Thomas Stephens <tmsteph1290@gmail.com>
2023-11-29 02:56:15 +0000

    Branded 3dvr title

--- app/page.jsx
+++ app/page.jsx
@@ -29,9 +29,9 @@ export default function Page() {
       <div className='mx-auto flex w-full flex-col flex-wrap items-center md:flex-row  lg:w-4/5'>
         {/* jumbo */}
         <div className='flex w-full flex-col items-start justify-center p-12 text-center md:w-2/5 md:text-left'>
-          <p className='w-full uppercase'>Next + React Three Fiber</p>
-          <h1 className='my-4 text-5xl font-bold leading-tight'>Next 3D Starter</h1>
-          <p className='mb-8 text-2xl leading-normal'>A minimalist starter for React, React-three-fiber and Threejs.</p>
+          <p className='w-full uppercase'>3dvr.tech</p>
+          <h1 className='my-4 text-5xl font-bold leading-tight'>We believe the future of the web is in 3d Virtual Reality websites.</h1>
+          <p className='mb-8 text-2xl leading-normal'>We use React, React-three-fiber and Threejs to create websites like this!</p>
         </div>
 
         <div className='w-full text-center md:w-3/5'>
